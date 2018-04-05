9962362ab1d5c6e4146c6d7c7effccc7cb26995d
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-04-05 18:19:28 +0200

    Added changing opacity for the smoke.

--- Assets/SmokeBehaviour.cs
+++ Assets/SmokeBehaviour.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SmokeBehaviour : StateMachineBehaviour {
-    
-	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
+
+    private SpriteRenderer renderer;
+    private bool hasReachedTop = false;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        renderer = animator.gameObject.GetComponent<SpriteRenderer>();
+        renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0);
 	}
     
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
-	}
+        const float alphaTop = 0.5f;
+
+        float alphaDifference;
+        if(!hasReachedTop && renderer.color.a < alphaTop)
+        {
+            alphaDifference = alphaTop / 10;
+        } else
+        {
+            alphaDifference = alphaTop / 10 * -1;
+        }
+
+        float newAlpha = renderer.color.a + alphaDifference;
+        renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, newAlpha);
+
+        if (newAlpha >= alphaTop)
+        {
+            hasReachedTop = true;
+        }
+    }
 }
