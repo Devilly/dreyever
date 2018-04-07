d6b722435f70c9b3fc3acccd8990f28822966bc9
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-04-07 09:07:29 +0200

    Made the canvas resize functional.

---
+++ Assets/Resources/scripts/dead/CanvasResize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasResize : MonoBehaviour {
+    
+	void Start () {
+        RectTransform transform = GetComponent<RectTransform>();
+
+        float halfHeight = transform.sizeDelta.y / 2;
+        float verticalScale = Camera.main.orthographicSize / halfHeight;
+
+        float width = transform.sizeDelta.x;
+        float futureWidth = Camera.main.orthographicSize * 2 / Screen.height * Screen.width;
+        float horizontalScale = futureWidth / width;
+            
+        transform.localScale = new Vector3(transform.localScale.x * horizontalScale,
+            transform.localScale.y * verticalScale, 1);
+    }
+}
---
+++ Assets/Resources/scripts/dead/SpriteResize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteResize : MonoBehaviour {
+    
+	void Start () {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        float halfHeight = renderer.bounds.extents.y;
+        float scaleToScreenFit = Camera.main.orthographicSize / halfHeight;
+        transform.localScale = new Vector3(transform.localScale.x * scaleToScreenFit,
+                transform.localScale.y * scaleToScreenFit, 1);
+    }
+}
