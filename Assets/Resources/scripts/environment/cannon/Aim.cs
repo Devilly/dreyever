0f18447559cfc935a3348929f68f56f2ed76b711
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-04-29 07:58:58 +0200

    Added cannon barrel aim.

---
+++ Assets/Resources/scripts/environment/cannon/Aim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+
+    private Transform playerTransform;
+    private Transform barrelTransform;
+	
+	void Start () {
+        playerTransform = GameObject.Find("Dreyever").transform;
+        barrelTransform = transform.Find("Barrel");
+	}
+	
+	void Update () {
+        Vector3 position = transform.position;
+        Vector3 playerPosition = playerTransform.position;
+
+        barrelTransform.rotation = Quaternion.FromToRotation(Vector3.down,
+            new Vector3(playerPosition.x - position.x, playerPosition.y - position.y, 0));
+	}
+}
