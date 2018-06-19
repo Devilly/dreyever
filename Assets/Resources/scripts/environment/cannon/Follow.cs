6ff42f26150cec35dbe7c7d042b5eb7f68f7c7fb
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-06-19 11:40:25 +0200

    Added following cannon rail.

---
+++ Assets/Resources/scripts/environment/cannon/Follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment.Cannon
+{
+    public class Follow : MonoBehaviour
+    {
+        private Transform playerTransform;
+
+        public float minimumPosition;
+        public float maximumPosition;
+
+        void Start()
+        {
+            playerTransform = GameObject.Find("Dreyever").transform;
+        }
+
+        void Update()
+        {
+           transform.localPosition = new Vector3(Mathf.Clamp(transform.parent.InverseTransformPoint(playerTransform.position).x, minimumPosition, maximumPosition), transform.localPosition.y, transform.localPosition.z);
+        }
+    }
+}
