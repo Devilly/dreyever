2e6ba8f2ae1b5bf88dd997deb1086b3b4fef2477
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-06-20 10:41:48 +0200

    Added hover and bling effect to green coin.

---
+++ Assets/Resources/scripts/environment/coins/Aesthetics.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment.Coins
+{
+    public class Aesthetics : MonoBehaviour
+    {
+        private Animator animator;
+
+        private float defaultPositionY;
+        private float sineTime;
+        private float previousSineTime;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+
+            defaultPositionY = transform.position.y;
+            sineTime = Mathf.PI;
+            previousSineTime = Mathf.PI;
+        }
+
+        void Update()
+        {
+            sineTime += 2 * Time.deltaTime;
+            sineTime = sineTime % (2 * Mathf.PI);
+
+            float valueToBling = .3f * Mathf.PI;
+            if (previousSineTime < valueToBling  && sineTime > valueToBling)
+            {
+                animator.Play("bling");
+            }
+
+            transform.position = new Vector3(transform.position.x,
+                defaultPositionY + Mathf.Sin(sineTime) * .25f,
+                transform.position.z);
+        }
+
+        void LateUpdate()
+        {
+            previousSineTime = sineTime;
+        }
+    }
+}
