394d0cbc816fcea24fb01974fe0b067c64e199a4
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-06-26 08:08:17 +0200

    Moved smoke animation to a more generic place, added coin interaction.

---
+++ Assets/Resources/scripts/environment/coins/CoinBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "Hitbox")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
