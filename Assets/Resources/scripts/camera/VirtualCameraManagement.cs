13b53fe6b780dd15f4c8f821a205784ae90a7949
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-07-03 16:20:05 +0200

    Use different virtual camera's for left and right movement.

---
+++ Assets/Resources/scripts/camera/VirtualCameraManagement.cs
@@ -0,0 +1,24 @@
+﻿using Dreyever;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualCameraManagement : MonoBehaviour {
+
+    public State state;
+
+    public GameObject virtualCameraRight;
+    public GameObject virtualCameraLeft;
+	
+	void Update () {
+		if(state.GetDirection() == Direction.RIGHT)
+        {
+            virtualCameraRight.SetActive(true);
+            virtualCameraLeft.SetActive(false);
+        } else
+        {
+            virtualCameraRight.SetActive(false);
+            virtualCameraLeft.SetActive(true);
+        }
+	}
+}
