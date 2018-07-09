be4d7d8926095d6cb6f96a0cdd3d569a6cd36b37
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-07-09 21:50:08 +0200

    Re-added debug continue-to-scene class.

---
+++ Assets/Resources/scripts/_debug/_Continue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _Continue : MonoBehaviour
+{
+    public string targetScene;
+
+	void Start () {
+        StartCoroutine(Do());
+	}
+	
+	IEnumerator Do () {
+		yield return new WaitForSeconds(1);
+
+        SceneManager.LoadSceneAsync(targetScene);
+	}
+}
