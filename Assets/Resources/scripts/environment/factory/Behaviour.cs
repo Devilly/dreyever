db8199a04973f95ae1498bb19b56f79c9052cdce
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-07-24 09:38:39 +0200

    Start dreyever with factory touch.

--- Assets/Resources/scripts/environment/factory/Behaviour.cs
+++ Assets/Resources/scripts/environment/factory/Behaviour.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using Dreyever;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Environment.Factory
 {
@@ -8,10 +10,55 @@ namespace Environment.Factory
     {
         public GameObject dreyeverPrefab;
         public Sprite[] animationSprites;
-        
+
+        private State state;
+        private new SpriteRenderer renderer;
+
+        private bool activated = false;
+
         void Start()
         {
-            Instantiate(dreyeverPrefab, transform.position, Quaternion.identity);
+            GameObject dreyever = Instantiate(dreyeverPrefab, transform.position, Quaternion.identity);
+            state = dreyever.transform.Find("Monocar").GetComponent<State>();
+
+            renderer = GetComponent<SpriteRenderer>();
+        }
+
+        void FixedUpdate()
+        {
+            if (activated) return;
+            if (Input.touches.Length == 0) return;
+
+            Touch touch = Input.GetTouch(0);
+            Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+
+            if(hit.collider != null)
+            {
+                if(hit.collider.name == "Factory")
+                {
+                    StartCoroutine(StartAnimation());
+                    activated = true;
+                }
+            }
+        }
+
+        private IEnumerator StartAnimation()
+        {
+            float frameTime = 1 / 40;
+            foreach (Sprite sprite in animationSprites)
+            {
+                yield return new WaitForSeconds(frameTime);
+                renderer.sprite = sprite;
+            }
+
+            StartCoroutine(StartMovingRight());
+        }
+
+        private IEnumerator StartMovingRight()
+        {
+            yield return new WaitForSecondsRealtime(2);
+            state.StartMovingRight();
         }
     }
 }
