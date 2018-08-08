6684bda62d1f39b329477459181b1e43823e873d
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-08-08 20:59:45 +0200

    Let coins in menu blink randomly.

---
+++ Assets/Resources/scripts/menu/CoinsBlink.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsBlink : MonoBehaviour {
+
+    private Image image;
+    public Sprite[] animationSprites;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+
+        StartCoroutine(TriggerAnimation());
+    }
+
+    private IEnumerator TriggerAnimation()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(3, 15));
+
+            StartCoroutine(Animate());
+        }
+    }
+
+    private IEnumerator Animate()
+    {
+        float frameTime = 1 / 40;
+        foreach (Sprite sprite in animationSprites)
+        {
+            yield return new WaitForSeconds(frameTime);
+            image.sprite = sprite;
+        }
+    }
+}
