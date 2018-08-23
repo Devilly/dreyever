53ffc1178462b69314ce0ab1fb01f4c4db92d8c1
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-08-23 10:53:14 +0200

    Make dreyever shadows with shaders instead of sprite mask.

--- Assets/Resources/scripts/start/WallOfDreyevers.cs
+++ Assets/Resources/scripts/start/WallOfDreyevers.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 public class WallOfDreyevers : MonoBehaviour {
 
     public Sprite[] dreyevers;
-    public Sprite overlayedColor;
-    [Range(0, 1)]
-    public float opacityForOverlayedColor;
+
+    public Material spriteMaterial;
 
     public int numberOfRows;
     public int numberOfColumns;
@@ -31,36 +30,27 @@ public class WallOfDreyevers : MonoBehaviour {
             for(int columnIndex = 0; columnIndex < numberOfColumns; columnIndex++)
             {
                 GameObject dreyeverShadow = new GameObject();
-                SpriteMask mask = dreyeverShadow.AddComponent<SpriteMask>();
-                mask.sprite = dreyevers[random.Next(0, dreyevers.Length)];
-                float resizeBy = columnWidth / (new float[] { mask.bounds.size.x, mask.bounds.size.y }).Max();
+                SpriteRenderer spriteRenderer = dreyeverShadow.AddComponent<SpriteRenderer>();
+                spriteRenderer.material = spriteMaterial;
+                spriteRenderer.sprite = dreyevers[random.Next(0, dreyevers.Length)];
+                float resizeBy = columnWidth / (new float[] { spriteRenderer.bounds.size.x, spriteRenderer.bounds.size.y }).Max();
                 dreyeverShadow.transform.localScale = new Vector3(dreyeverShadow.transform.localScale.x * resizeBy,
                     dreyeverShadow.transform.localScale.y * resizeBy, 1);
 
-                float centerX = mask.sprite.rect.width / 2;
-                float pivotX = mask.sprite.pivot.x;
-                float offsetX = (pivotX - centerX) / mask.sprite.rect.width;
+                float centerX = spriteRenderer.sprite.rect.width / 2;
+                float pivotX = spriteRenderer.sprite.pivot.x;
+                float offsetX = (pivotX - centerX) / spriteRenderer.sprite.rect.width;
 
-                float centerY = mask.sprite.rect.height / 2;
-                float pivotY = mask.sprite.pivot.y;
-                float offsetY = (pivotY - centerY) / mask.sprite.rect.height;
+                float centerY = spriteRenderer.sprite.rect.height / 2;
+                float pivotY = spriteRenderer.sprite.pivot.y;
+                float offsetY = (pivotY - centerY) / spriteRenderer.sprite.rect.height;
 
                 float marginAsCellSizeFraction = .75f;
                 dreyeverShadow.transform.position = new Vector3(
-                    topLeftCorner.x + (columnIndex + marginAsCellSizeFraction) * columnWidth + mask.bounds.size.x * offsetX,
-                    topLeftCorner.y - (rowIndex + marginAsCellSizeFraction) * rowHeight + mask.bounds.size.y * offsetY,
+                    topLeftCorner.x + (columnIndex + marginAsCellSizeFraction) * columnWidth + spriteRenderer.bounds.size.x * offsetX,
+                    topLeftCorner.y - (rowIndex + marginAsCellSizeFraction) * rowHeight + spriteRenderer.bounds.size.y * offsetY,
                     0);                
             }
         }
-
-        GameObject maskedOverlay = new GameObject();
-        SpriteRenderer renderer = maskedOverlay.AddComponent<SpriteRenderer>();
-        renderer.sprite = overlayedColor;
-        renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, opacityForOverlayedColor);
-        renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
-        maskedOverlay.transform.localScale = new Vector3((bottomRightCorner.x - topLeftCorner.x) / renderer.bounds.size.x,
-            (topLeftCorner.y - bottomRightCorner.y) / renderer.bounds.size.y, 1);
-        maskedOverlay.transform.position = new Vector3((bottomRightCorner.x + topLeftCorner.x) / 2,
-            (bottomRightCorner.y + topLeftCorner.y) / 2, 0);
     }
 }
