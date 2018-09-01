8913eec80e7a29b1a9637481620064e787dafc49
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2018-09-01 07:59:37 +0200

    Added dreyever name and properties to garage.

---
+++ Assets/Resources/scripts/menu/DreyeverPropertiesListing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DreyeverPropertiesListing : MonoBehaviour {
+
+    private Text textObject;
+
+    void Start()
+    {
+        textObject = GetComponent<Text>();
+
+        SetText();
+    }
+
+	void Update () {
+        SetText();
+    }
+
+    private void SetText()
+    {
+        List<string> properties = new List<string>();
+        Scriptables.Dreyevers.Dreyever currentDreyever = Persistent.Environment.instance.GetCurrentDreyever();
+
+        if(currentDreyever.canAirJump)
+        {
+            properties.Add("Airjump");
+        }
+
+        textObject.text = string.Join(System.Environment.NewLine, properties.ToArray());
+    }
+}
